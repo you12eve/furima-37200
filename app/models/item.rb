54c6052dc429961a48526ca6dcffd0dd6766a4ba
you12eve <photo.you24d@gmail.com>
2022-04-07 07:40:20 +0900

    imageの必須

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,9 +16,16 @@ class Item < ApplicationRecord
   validates :charge_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :source_id,        presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :scheduled_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :image,            presence: true, unless: :was_attached?
 
   has_one_attached :image
   belongs_to :user
 
   #has_one :purchase
+
+
+
+  def was_attached?
+    self.image.attached?
+  end
 end
