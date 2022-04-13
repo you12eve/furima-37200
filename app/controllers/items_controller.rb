58acbec8dc7586a168881f0bb4b49e1827c261a2
you12eve <photo.you24d@gmail.com>
2022-04-13 11:19:22 +0900

    商品情報削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :move_to_index]
   def index
     @items = Item.all.order('created_at DESC')
@@ -32,7 +32,12 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
-
+  def destroy
+    item = Item.find(params[:id])
+    if item.destroy
+      redirect_to root_path
+    end
+  end
   private
 
   def item_params
